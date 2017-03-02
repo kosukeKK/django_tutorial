5ece3058afb0d5301415ed3c68c1f05cde3dff41
kosuke.kato <kosuke.kato@jmpc-041.local>
2017-03-02 19:31:35 +0900

    djangotutorial

---
+++ mysite/polls/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import Question
+from .models import Choice
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('pk', 'question_text', 'pub_date')
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
